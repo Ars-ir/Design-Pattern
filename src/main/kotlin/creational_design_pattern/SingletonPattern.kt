be325281d0bf52fab19f2f988645198953cfda9b
Arfan Siregar <arfan.siregar@kvendor.id>
2023-11-28 23:16:48 +0700

    Creating Kdoc for SingletonPattern

--- src/main/kotlin/creational_design_pattern/SingletonPattern.kt
+++ src/main/kotlin/creational_design_pattern/SingletonPattern.kt
@@ -2,6 +2,14 @@ package creational_design_pattern
 
 import java.util.regex.Pattern
 
+/**
+ * Singleton is a creational design pattern that lets you ensure
+ * that a class has only one instance, while providing a global access
+ * point to this instance.
+ *
+ * @author arfan
+ * */
+
 object ValidateName {
     fun getNameWithRegex(fullName: String) = Pattern.compile("[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
             "\\@" +
